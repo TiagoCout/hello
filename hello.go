823a027ba80393d41aa5e9eab9066a5bdf0042ed
TiagoCout <tiagolcout@gmail.com>
2024-06-17 18:02:00 +0100

    Adding my first Go project on Git

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Tiago"
+	var version float32 = 1.1
+	fmt.Println("Hello, Sr.", name)
+	fmt.Println("The version program is:", version)
+	
+}
